82e4b58aa8eb1db374c89f44aa7af0e32c138dae
Anthony <anthony.a.s.martin@gmail.com>
2021-04-01 15:57:36 -0400

    solve phase 2

--- storage-project.js
+++ storage-project.js
@@ -11,10 +11,23 @@ window.addEventListener("DOMContentLoaded", (event) => {
   };
 
   const showCart = () => {
-    
+    const shoppingCart = document.getElementById('shopping-cart');
+    const ul = document.createElement('ul');
+    for(let key in localStorage){
+      const item = key;
+      const quantity = localStorage.getItem(key);
+      const li = document.createElement('li');
+      // if(quantity){
+        li.innerText = "item: " + item + ", quantity: " + quantity;
+        ul.appendChild(li); 
+      // }
+    }
+    shoppingCart.appendChild(ul);
+
   };
 
   storeItem();
+  showCart();
 
 
       // const removeItem = () => {
