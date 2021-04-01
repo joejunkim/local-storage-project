0f6182a0b2971a5bd1da2d52b114be94730ebfe8
Anthony <anthony.a.s.martin@gmail.com>
2021-04-01 13:42:38 -0400

    start project

--- storage-project.js
+++ storage-project.js
@@ -1,15 +1,28 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  
+  let formStorage = () => {
+    const form = document.getElementById('.form form__container');
 
+    form.addEventListener('click', (event)=>{
+      const value = document.getElementById('items')
+      value.innerText = event.target.value;
+
+      localStorage.setItem("item",value)
+    })
   // const showCart = () => {
-
-  // };
-
-  // const storeItem = () => {
-
-  // };
-
-  // const removeItem = () => {
-
-  // };
-
-});
+    
+    // };
+    
+    // const storeItem = () => {
+      
+      // };
+      
+      // const removeItem = () => {
+        
+        // };
+       
+        
+      }
+      formStorage()
+      });
+      
